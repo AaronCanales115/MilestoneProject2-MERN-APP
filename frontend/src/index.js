be4dc8f8c94ef8509a87ecb26d3b798d0c238def
Aaron Canales <aaroncanales0107@gmail.com>
2023-08-31 19:09:37 -0700

    scss added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+//import './index.css';
+import './css/style.css'
 import App from './App';
 import {FoodContextProvider} from './context/foodContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
